535f01f2146f241bafe962db193300bb55309d02
soni1541 <kurbatovasoni5@gmail.com>
2023-10-10 14:49:21 +0500

    Созданы экраны

--- app/src/main/java/com/example/lr4_day/MainActivity.java
+++ app/src/main/java/com/example/lr4_day/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lr4_day;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void screen_Morning(View view) {
+        Intent intent = new Intent(MainActivity.this, MorningActivity.class);
+        startActivity(intent);
+    }
+    public void screen_Day(View view) {
+        Intent intent = new Intent(MainActivity.this, DayActivity.class);
+        startActivity(intent);
+    }
+    public void screen_Evening(View view) {
+        Intent intent = new Intent(MainActivity.this, EveningActivity.class);
+        startActivity(intent);
+    }
+    public void screen_Night(View view) {
+        Intent intent = new Intent(MainActivity.this, NightActivity.class);
+        startActivity(intent);
     }
 }
