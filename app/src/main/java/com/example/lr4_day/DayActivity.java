cac6a7098d5091a9a0b62940cb5db19747149c5d
soni1541 <kurbatovasoni5@gmail.com>
2023-10-10 14:58:00 +0500

    Сделан ДЕНЬ

--- app/src/main/java/com/example/lr4_day/DayActivity.java
+++ app/src/main/java/com/example/lr4_day/DayActivity.java
@@ -2,7 +2,9 @@ package com.example.lr4_day;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DayActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class DayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day);
+    }
+
+    public void screen_Evening(View view) {
+        Intent intent = new Intent(DayActivity.this, EveningActivity.class);
+        startActivity(intent);
     }
 }
